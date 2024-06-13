c0900f7958ee78d067ccf24fbf6baf032659d48d
Joshua Warner <jw4846@princeton.edu>
2024-06-13 12:59:37 -0400

    examples compile and dont fail

--- cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
+++ cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
@@ -3,27 +3,27 @@
 cmake_policy(SET CMP0009 NEW)
 
 # SRC at CMakeLists.txt:7 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/dp9443/Documents/Princeton/untitled/*.c")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/joshuawarner/Downloads/Summer JUCE/untitled/*.c")
 set(OLD_GLOB
-  "/Users/dp9443/Documents/Princeton/untitled/adder.c"
-  "/Users/dp9443/Documents/Princeton/untitled/mapping.c"
-  "/Users/dp9443/Documents/Princeton/untitled/processor.c"
-  "/Users/dp9443/Documents/Princeton/untitled/tester.c"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/adder.c"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/mapping.c"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/processor.c"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/tester.c"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "/Users/dp9443/Documents/Princeton/untitled/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "/Users/joshuawarner/Downloads/Summer JUCE/untitled/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
 
 # SRC at CMakeLists.txt:7 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/dp9443/Documents/Princeton/untitled/*.h")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "/Users/joshuawarner/Downloads/Summer JUCE/untitled/*.h")
 set(OLD_GLOB
-  "/Users/dp9443/Documents/Princeton/untitled/adder.h"
-  "/Users/dp9443/Documents/Princeton/untitled/defs.h"
-  "/Users/dp9443/Documents/Princeton/untitled/mapping.h"
-  "/Users/dp9443/Documents/Princeton/untitled/processor.h"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/adder.h"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/defs.h"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/mapping.h"
+  "/Users/joshuawarner/Downloads/Summer JUCE/untitled/processor.h"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "/Users/dp9443/Documents/Princeton/untitled/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "/Users/joshuawarner/Downloads/Summer JUCE/untitled/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
--- tester.c
+++ tester.c
@@ -23,11 +23,11 @@ int main(void)
 
     processor1.processorID = 1; // ID for this processor
     processor1.object = &adder1; // Processor's aliased Leaf object
-    processor1.tick = &tAdder_tick; // The object's tick function
+    processor1.tick = (tTickFunc) &tAdder_tick; // The object's tick function
     processor1.numSetterFunctions = 2; // Number of setter functions used by
                                        // the processor's object
-    processor1.setterFunctions[1] = &tAdder_setA;
-    processor1.setterFunctions[2] = &tAdder_setB;
+    processor1.setterFunctions[0] = (tSetter) &tAdder_setA;
+    processor1.setterFunctions[1] = (tSetter) &tAdder_setB;
 
     processor1.inParameters[1] = 10; // IN sources for the processor
 
@@ -39,11 +39,11 @@ int main(void)
 
     processor2.processorID = 2; // ID for this processor
     processor2.object = &adder2; // Processor's aliased Leaf object
-    processor2.tick = &tAdder_tick; // The object's tick function
+    processor2.tick = (tTickFunc) &tAdder_tick; // The object's tick function
     processor2.numSetterFunctions = 2; // Number of setter functions used by
     // the processor's object
-    processor2.setterFunctions[1] = &tAdder_setA;
-    processor2.setterFunctions[2] = &tAdder_setB;
+    processor2.setterFunctions[0] = (tSetter) &tAdder_setA;
+    processor2.setterFunctions[1] = (tSetter) &tAdder_setB;
 
     processor2.inParameters[1] = 30; // IN sources for the processor
 
