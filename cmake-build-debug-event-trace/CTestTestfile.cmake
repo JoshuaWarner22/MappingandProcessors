1796a9690e4e763da95a5bf1d1e20856fcde5a50
Davis Polito <davispolito1@gmail.com>
2024-08-08 12:25:42 -0400

    fixxing git errors

--- cmake-build-debug-event-trace/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dp9443/Documents/Princeton/MappingandProcessors
-# Build directory: /Users/dp9443/Documents/Princeton/MappingandProcessors/cmake-build-debug-event-trace
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LEAF/leaf")
-subdirs("processors")
-subdirs("test")
--- cmake-build-debug-event-trace/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dp9443/Documents/Princeton/MappingandProcessors/test
-# Build directory: /Users/dp9443/Documents/Princeton/MappingandProcessors/cmake-build-debug-event-trace/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/dp9443/Documents/Princeton/MappingandProcessors/cmake-build-debug-event-trace/test/_tests_include-b12d07c.cmake")
