823915ababfc2c83472a9c36d9939b1ec95614ff
Davis Polito <davispolito1@gmail.com>
2024-11-19 18:22:03 -0500

    adding envmoduole

--- processors/funcmaps.h
+++ processors/funcmaps.h
@@ -4,18 +4,20 @@
 
 #ifndef UNTITLED_FUNCMAPS_H
 #define UNTITLED_FUNCMAPS_H
-//#include "EnvModule.h"
+#include "EnvModule.h"
 //#include "LFOModule.h"
 #include "FilterModule.h"
 #include "SimpleOscModule.h"
 //each file that includes this will get its own copy of it. this wastes memory. Should find a singleton style solution
 static leaf::createProcFunc proc_init_map[] = {
                         (leaf::createProcFunc)tOscModule_processorInit,
-                        (leaf::createProcFunc)tFiltModule_processorInit
+                        (leaf::createProcFunc)tFiltModule_processorInit,
+    (leaf::createProcFunc)tEnvModule_processorInit
                         };
 static leaf::createModuleFunc module_init_map[] = {
                         (leaf::createModuleFunc)tOscModule_init,
-                        (leaf::createModuleFunc)tFiltModule_init
+                        (leaf::createModuleFunc)tFiltModule_init,
+                            (leaf::createModuleFunc)tEnvModule_init
                         };
 
 
